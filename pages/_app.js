db8a2bd373853511fdf1b44b59399cd8f3cf8ca9
Kumar Manas <singularity@Kumars-MacBook-Pro.local>
2022-05-13 02:40:15 +0530

    hotjar is added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/font-awesome/css/font-awesome.min.css';
+import { hotjar } from 'react-hotjar'
+import { useEffect } from 'react'
 
 
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    hotjar.initialize(2967802, 6)
+  }, [])
   return <Component {...pageProps} />
 }
 
